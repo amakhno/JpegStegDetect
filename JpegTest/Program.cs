ee4f45213a6045062b4d362143e5eeaeb68264bd
Artem Makhno <artem_mahno@mail.ru>
2017-11-12 19:42:06 +0300

    Accord

--- JpegTest/Program.cs
+++ JpegTest/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using Accord.Controls;
+using Accord.IO;
+using Accord.Math;
+using Accord.Statistics.Distributions.Univariate;
+using Accord.MachineLearning.Bayes;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Statistics.Kernels;
+using System.Data;
 
 namespace JpegTest
 {
@@ -12,11 +15,28 @@ namespace JpegTest
     {
         static void Main(string[] args)
         {
-            DCMatrix matrix = new DCMatrix("cat.jpg");
+            DCMatrix matrix;
             DirectoryInfo d = new DirectoryInfo(@".\images");//Assuming Test is your Folder
-            FileInfo[] Files = d.GetFiles("*.jpg"); //Getting Text files
-            var stat = matrix.GetStat();
-            DCMatrix matrix2 = new DCMatrix("cat(1).jpg");
+            FileInfo[] Files = d.GetFiles("*.jp?g"); //Getting Text files
+            double[][] inputs = new double[Files.Length][];
+            double[] outputs = new double[Files.Length];
+            //Accord
+            var teacher = new SequentialMinimalOptimization<Gaussian>()
+            {
+                UseComplexityHeuristic = true,
+                UseKernelEstimation = true // estimate the kernel from the data
+            };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                matrix = new DCMatrix(Files[i].FullName);
+                var stat = matrix.GetStat();
+                inputs[i] = stat;
+                outputs[i] = 0;
+            }
+
+
+            var svm = teacher.Learn(inputs, outputs);
             Console.ReadKey(true);
         }
 
@@ -24,7 +44,7 @@ namespace JpegTest
         {
             string output = "";
 
-            for(int k = 0; k<((Y.Length>10)?10:Y.Length) ; k++)
+            for (int k = 0; k < ((Y.Length > 10) ? 10 : Y.Length); k++)
             {
                 for (int i = 0; i < Y[k].GetLength(0); i++)
                 {
